9effcbf3a6b06ed73df22fcbb2fc329d4fa549dc
Artem-work <artem-work@mail.ru>
2019-07-18 15:03:36 +0300

    add default phone number and updated db

--- smsapp/views.py
+++ smsapp/views.py
@@ -18,10 +18,13 @@ def add_and_save(request):
             current_text = smsf.cleaned_data['text']
             # получаю систему выбранную в форме
             sys = System.objects.get(pk=current_system_id)
-            # sys_default = System.objects.get(name='Default')
-            # получаю список телефонов по выбранной системк
+            # получаю список телефонов по выбранной системе
             current_phone_list = [cont.phone for cont in sys.contact_set.all()]
-            # default_phone_list = [cont.phone for cont in sys_default.contact_set.all()]
+            # если выбрана не тестовая группа, то список телефонов дополняется телефонами из группы Default
+            if sys.name != 'TEST':
+                sys_default = System.objects.get(name='Default')
+                default_phone_list = [cont.phone for cont in sys_default.contact_set.all()]
+                current_phone_list += default_phone_list
             new_sms_form.status = send_sms_and_get_status(current_phone_list, current_text)
             new_sms_form.save()
             context = {'status': new_sms_form.status}
